938f0627ccc8c8cc30632af4522e5af616529d00
joedono <zidane320@yahoo.com>
2017-11-20 17:32:49 -0500

    Started working on bullet death effect

--- weapon/manager-weapon.lua
+++ weapon/manager-weapon.lua
@@ -4,10 +4,16 @@ require "weapon/weapon-bullet-pickup";
 ManagerWeapon = Class {
   init = function(self, player)
     self.weapons = {};
+    self.effects = {};
     self.player = player;
 
     self.imageBullet = love.graphics.newImage("asset/image/bullet.png");
     self.imageBulletPickup = love.graphics.newImage("asset/image/bullet-pickup.png");
+
+    local partImage = love.graphics.newImage("asset/image/effect-bullet-death.png");
+    local ps = love.graphics.newParticleSystem(partImage, 50);
+    -- TODO build enemy death effect
+    self.bulletDeathEffect = ps;
   end
 };
 
@@ -20,6 +26,12 @@ function ManagerWeapon:spawnBullet(bx, by, type)
 end
 
 function ManagerWeapon:update(dt)
+  self:updateWeapons(dt);
+  self:updateEffects(dt);
+
+end
+
+function ManagerWeapon:updateWeapons(dt)
   local activeWeapons = {};
 
   for index, weapon in ipairs(self.weapons) do
@@ -27,10 +39,28 @@ function ManagerWeapon:update(dt)
 
     if weapon.active then
       table.insert(activeWeapons, weapon);
-    end
+    else
+      local ps = self.bulletDeathEffect:clone();
+      ps:setPosition(weapon.box.x + weapon.box.w / 2, weapon.box.y + weapon.box.h / 2);
+      ps:emit(50);
+      table.insert(self.effects, ps);
   end
 
   self.weapons = activeWeapons;
+end
+
+function ManagerWeapon:updateEffects(dt)
+  local activeEffects = {};
+
+  for index, effect in ipairs(self.effects) do
+    effect:update(dt);
+
+    if effect:getCount() > 0 then
+      table.insert(activeEffects, effect);
+    end
+  end
+
+  self.effects = activeEffects;
 end
 
 function ManagerWeapon:draw()
